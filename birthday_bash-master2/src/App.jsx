5af59ad96cd34f9fddcf933704bebf6caaa7c8eb
sanjaykumar SS <118790353+sanjaykumar-SS12@users.noreply.github.com>
2024-01-24 10:07:01 +0530

    .

---
+++ birthday_bash-master2/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hello_World from './components/Hello_World'
+import { Routes ,Route, Link} from 'react-router-dom'
+import NavBar from './components/NavBar/NavBar'
+
+export default function App() {
+  return (
+    <>
+      
+        <NavBar />
+      <Routes>
+
+        
+
+      </Routes>
+ 
+      
+    </>
+  )
+}
---
+++ birthday_bash-master2/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./nav.css"
+export default function NavBar() {
+  return (
+    <>
+    <div className="nav-bar-wrap">
+        
+        <div className='logo'>
+            <h1>Birthday bash</h1>
+        </div>
+        <div className='content'>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Page</li>
+            <li>Contact</li>
+        </ul>
+
+        </div>
+       
+
+    </div>
+      
+    </>
+  )
+}
